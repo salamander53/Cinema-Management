66c65ec8f33838e8ea5df0839f718a49677c97ad
salamander53 <dotruongkhoa04@gmail.com>
2024-12-06 12:39:23 +0700

    Khoa

---
+++ Cinema_Management/Cinema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[Cinema]
+(
+  Cinema_ID INT IDENTITY(1,1) PRIMARY KEY,
+  -- Mã rạp tự tăng
+  Name NVARCHAR(100) NOT NULL,
+  -- Tên rạp chiếu
+  Address NVARCHAR(255) NOT NULL,
+  -- Địa chỉ
+);
+
+SELECT * FROM Cinema

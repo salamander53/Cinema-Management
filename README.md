9998bb54432e3fde726b92d5218575ea38fbd2df
salamander53 <dotruongkhoa04@gmail.com>
2024-12-12 13:38:37 +0700

    up

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# BTL_DATABASE
-<<<<<<< HEAD
-viết thêm dòng này để up lên git cho vui 
-=======
+# React + Vite
 
-khang
->>>>>>> f1381aadb733c67fea32d467460b4e8345a3d8db
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Home from "./HomePage.jsx";
+import Employee from "./employee";
+import Cinema from "./Cinema";
+
+import ShiftManage from "./ShiftManage";
+import JobManage from "./jobmanage";
+import SalaryManagement from "./SalaryManagement";
+import SalaryPay from "./SalaryPay";
+import AddJob from "./Addjob";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import ProtectedRoute from "./Components/ProtectedRoute";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/Cinema" element={<Cinema />} />
+          <Route path="/employee" element={<Employee />} />
+          <Route path="/jobmanage" element={<JobManage />} />
+
+          <Route path="/SalaryManagement" element={<SalaryManagement />} />
+          <Route path="/AddJob" element={<AddJob />} />
+          <Route path="/ShiftManage" element={<ShiftManage />} />
+          <Route path="/SalaryPay" element={<SalaryPay />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
